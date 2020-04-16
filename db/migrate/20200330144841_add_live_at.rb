76652058af4375eeb431ab624b5310f2cc5c4a87
Ryan Hickman <kingsrborn@gmail.com>
2020-04-16 19:30:45 -0400

    sdf

--- db/migrate/20200330144841_add_live_at.rb
+++ db/migrate/20200330144841_add_live_at.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddLiveToRooms < ActiveRecord::Migration[5.0]
+class AddLiveToRoom < ActiveRecord::Migration[5.0]
   def change
     add_column :rooms, :live_at, :datetime
   end
